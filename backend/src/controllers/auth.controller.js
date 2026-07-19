8e1f92193674cc9febcfc56c1774324991c46d14
aziznqd <aziznaqdiyev@gmail.com>
2026-07-19 17:00:43 +0400

    login logout function added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,4 +55,39 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 
-}
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const isPassswordValid = await bcrypt.compare(password, user.password);
+        if(!isPassswordValid){
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+
+    }catch(err){
+        console.error("Error during login:", err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({ message: "Logged out successfully" });
+};
